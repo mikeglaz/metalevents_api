3845e0aedf018daa217261f37b4e4c2dc6d1cc26
Mike Glaz <mikeglaz@yahoo.com>
2020-02-18 12:40:35 -0600

    updated events fixture

--- db/fixtures/events.rb
+++ db/fixtures/events.rb
@@ -1,10 +1,12 @@
+date = DateTime.current
+
 Event.seed do |s|
   s.id = 1
   s.user_id = 1
   s.venue_id = 3
-  s.name = 'Abigor'
-  s.description = 'Nachthymnen Tour'
-  s.date = DateTime.new(2019, 12, 29, 19, 0, 0)
+  s.name = 'Slayer'
+  s.description = 'The Tour To End All Tours'
+  s.date = DateTime.new(date.year, date.month - 1, date.last_month.end_of_month.day, 19, 0, 0)
 end
 
 Event.seed do |s|
@@ -13,7 +15,7 @@ Event.seed do |s|
   s.venue_id = 3
   s.name = 'Ghost'
   s.description = 'A Pale Tour Named Death'
-  s.date = DateTime.new(2020, 1, 1, 19, 0, 0)
+  s.date = DateTime.new(date.year, date.month, 1, 19, 0, 0)
 end
 
 Event.seed do |s|
@@ -21,8 +23,8 @@ Event.seed do |s|
   s.user_id = 2
   s.venue_id = 1
   s.name = 'Metallica'
-  s.description = 'World Tour 2019'
-  s.date = DateTime.new(2020, 1, 31, 20, 0, 0)
+  s.description = "Justice For All Tour #{date.year}"
+  s.date = DateTime.new(date.year, date.month, 12, 19, 0, 0)
 end
 
 Event.seed do |s|
@@ -30,24 +32,42 @@ Event.seed do |s|
   s.user_id = 2
   s.venue_id = 2
   s.name = 'Arcturus'
-  s.description = 'Church Burning Tour 2019'
-  s.date = DateTime.new(2020, 2, 1, 20, 0, 0)
+  s.description = "Church Burning Tour #{date.year}"
+  s.date = DateTime.new(date.year, date.month, 20, 19, 0, 0)
 end
 
 Event.seed do |s|
   s.id = 5
-  s.user_id = 3
-  s.venue_id = 3
-  s.name = 'Opeth'
-  s.description = 'Blackwater Park Tour'
-  s.date = DateTime.new(2020, 2, 5, 21, 0, 0)
+  s.user_id = 2
+  s.venue_id = 2
+  s.name = 'Dissection'
+  s.description = "Somberlain Tour #{date.year}"
+  s.date = DateTime.new(date.year, date.month, 20, 19, 0, 0)
 end
 
 Event.seed do |s|
   s.id = 6
+  s.user_id = 2
+  s.venue_id = 2
+  s.name = 'Sentenced'
+  s.description = "Funeral Spring Tour #{date.year}"
+  s.date = DateTime.new(date.year, date.month, date.end_of_month.day, 19, 0, 0)
+end
+
+Event.seed do |s|
+  s.id = 7
+  s.user_id = 3
+  s.venue_id = 3
+  s.name = 'Opeth'
+  s.description = 'Blackwater Park Tour'
+  s.date = DateTime.new(date.year, date.month + 1, 1, 19, 0, 0)
+end
+
+Event.seed do |s|
+  s.id = 8
   s.user_id = 1
   s.venue_id = 3
   s.name = 'Animals As Leaders'
   s.description = 'Tempting Death Tour'
-  s.date = DateTime.new(2020, 3, 3, 19, 0, 0)
+  s.date = DateTime.new(date.year, date.month + 1, 3, 19, 0, 0)
 end
