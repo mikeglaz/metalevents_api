814b163c8844437327fd1530a8381ddf229b4a2c
Mike Glaz <mikeglaz@yahoo.com>
2020-10-25 11:51:22 -0500

    events#create done

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe "Events", type: :request do
         create(:event)
       }
 
+      let!(:user) {
+        create(:user)
+      }
+
+      let(:new_event) {
+        build(:event)
+      }
+
     it "events#index" do
       get events_path
       expect(response).to have_http_status(200)
@@ -21,9 +29,13 @@ RSpec.describe "Events", type: :request do
     end
 
     it "events#create" do
-      get event_path(event.id)
-      expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to eq(JSON.parse(event.to_json))
+      post login_path, params: { email: user.email, password: user.password }
+
+      headers = { 'Authorization': JSON.parse(response.body)['token'] }
+
+      post events_path, params: { event: new_event.attributes }, headers: headers
+      expect(response).to have_http_status(201)
+      # expect(JSON.parse(response.body)).to eq(JSON.parse(event.to_json))
     end
   end
 end
